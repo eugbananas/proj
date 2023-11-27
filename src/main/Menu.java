4d4846701e802908ba3f03918289bca42035f162
Jea Abogado <93728726+Jeacharmander@users.noreply.github.com>
2023-11-27 17:13:07 +0800

    edited menu

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -13,6 +13,21 @@ public class Menu extends JFrame{
         setSize(1280, 720);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        JTextField txtSearch = new JTextField();
+        txtSearch.setBounds(210, 347, 402, 51);
+        txtSearch.setBackground(Color.decode("#3B4252"));
+        txtSearch.setForeground(Color.WHITE);
+        txtSearch.setBorder(BorderFactory.createLineBorder(Color.decode("#3B4252")));
+
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Menu menu = new Menu();
+            menu.setVisible(true);
+        });
     }
 
 }
