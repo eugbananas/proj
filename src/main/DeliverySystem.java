6f6ce8c160e3afc2db6b9dbb42626946a5a552d5
Jea Abogado <93728726+Jeacharmander@users.noreply.github.com>
2023-12-11 23:05:29 +0800

    dfdfsdfdsf

---
+++ src/main/DeliverySystem.java
@@ -0,0 +1,12 @@
+package main;
+
+public class DeliverySystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
